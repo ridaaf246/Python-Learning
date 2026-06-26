3bdd70036e05424c76bf0556ed5dc16253e4f126
ridaaf246 <ridaaf246@gmail.com>
2026-06-26 11:32:15 -0700

    Add match-case statements for day and traffic light

--- Python All Practice Local File.py
+++ Python All Practice Local File.py
@@ -631,3 +631,29 @@ else:
 
     else:
         print("Best luck for next time, Your grade is 'F'")
+
+
+
+
+
+day = int(input("Enter 1st Number: \n"))
+match day:
+    case 1:
+        print("Monday")
+  
+    case _:
+        print("You enter another day.")
+
+
+color = input("Enter traffic light colors:").strip().lower()
+match color:
+    case "red":
+        print("Stop.")
+    case "green":
+         print("Go.")
+    case "yellow":
+        print("Get ready to go.")
+    case _:
+        print("You enter invalid color")
+
+
